897d1d4da6984d4fb0627a6c365168cd9e8db8de
Hanjin Tak <hjxarchive@gmail.com>
2026-01-27 15:43:38 +0900

    FIX: Programmatically remove Spline watermark via onLoad callback

--- frontend/src/components/Hero3D.tsx
+++ frontend/src/components/Hero3D.tsx
@@ -1,7 +1,33 @@
-import Spline from '@splinetool/react-spline';
+import Spline from '@splinetool/react-spline'
+import { useRef } from 'react'
 
-export default function App() {
+export default function Hero3D() {
+    const containerRef = useRef<HTMLDivElement>(null)
+
+    const handleLoad = () => {
+        // HACK: Programmatically remove the Spline watermark
+        setTimeout(() => {
+            const logo = document.querySelector('a[href^="https://spline.design"]')
+            if (logo) {
+                logo.remove()
+            }
+
+            // Also check inside the container just in case
+            if (containerRef.current) {
+                const internalLogo = containerRef.current.querySelector('a[href^="https://spline.design"]')
+                if (internalLogo) {
+                    internalLogo.remove()
+                }
+            }
+        }, 500) // Wait a bit for it to render
+    }
+
     return (
-        <Spline scene="https://prod.spline.design/BNDz9QWDD6sx1uxI/scene.splinecode" />
-    );
+        <div className="w-full h-[500px]" ref={containerRef}>
+            <Spline
+                scene="https://prod.spline.design/BNDz9QWDD6sx1uxI/scene.splinecode"
+                onLoad={handleLoad}
+            />
+        </div>
+    )
 }
